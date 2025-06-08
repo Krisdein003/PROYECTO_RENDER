1f8b456135e1d04dc344439be06340174762e55f
Kitty <krisdeinv@gmail.com>
2025-06-08 01:35:14 -0500

    Actualizar py

--- holamundo.py
+++ holamundo.py
@@ -1,6 +1,8 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
 @app.route("/")
 def holamundo():
@@ -9,5 +11,7 @@ def holamundo():
     <h2>Aplicación básica en la plataforma Render</h2>
     """
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
